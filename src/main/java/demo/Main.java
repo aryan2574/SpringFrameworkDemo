08b7bd4b1e70603b96eab38a2931919460d69034
aryan2574 <ayush.aryan94@yahoo.com>
2024-02-03 20:16:09 +0530

    Created beans using Java Configuration

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,17 +1,19 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Creation of context to use Spring dependency injection from xml
+//        Creation of context to use Spring dependency injection from xml
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context1 = new AnnotationConfigApplicationContext(BeanConfig.class);
 
         Doctor doctor = context.getBean(Doctor.class);
 
-        // Other ways to call Nurse object
+//        Other ways to call Nurse object
         Nurse nurse = (Nurse) context.getBean("nurse");
 
         Staff staff = context.getBean(Doctor.class);
@@ -21,15 +23,20 @@ public class Main {
         // Implemented using Interface
         staff.assist();
 
-        // Usage of constructor to input values in properties
-        // Usage of setter function to get the qualification
+//        Usage of constructor to input values in properties
+//        Usage of setter function to get the qualification
         Doctor doctor1 = context.getBean(Doctor.class);
         doctor1.assist();
         System.out.println(doctor1.getQualification());
 
-        // Pharmacist using Annotation
+//        Pharmacist using Annotation
         Pharmacist pharmacist = context.getBean(Pharmacist.class);
         pharmacist.assist();
+
+//        Using Java Configuration
+//        To use Java configuration with a class - either add @Component at top of it or add @Bean
+        Nurse nurse1 = context1.getBean(Nurse.class);
+        nurse1.assist();
 
     }
 }
